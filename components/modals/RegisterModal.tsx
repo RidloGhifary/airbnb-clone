e426f2a9f359c7190d3d33ea1291fcd87c3c2568
RidloGhifary <ridloghfry@gmail.com>
2024-08-08 15:44:12 +0700

    feat: register functionality & prisma setup

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -3,7 +3,7 @@
 import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import useRegisterModal from "@/hooks/useRegisterModal";
 import Modal from "./Modal";
