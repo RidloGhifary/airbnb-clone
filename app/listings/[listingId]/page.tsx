489343fbc7abe5dc5e26b91b1e19663b375bd791
RidloGhifary <ridloghfry@gmail.com>
2024-08-09 20:45:49 +0700

    feat: reservation functionality

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -2,6 +2,7 @@ import getCurrentUser from "@/actions/getCurrentUser";
 import getListingById from "@/actions/getListingById";
 import EmptyState from "@/components/EmptyState";
 import ListingClient from "./_components/ListingClient";
+import getReservation from "@/actions/getReservation";
 
 interface IParams {
   listingId?: string;
@@ -9,11 +10,18 @@ interface IParams {
 
 export default async function ListingPage({ params }: { params: IParams }) {
   const listing = await getListingById(params);
+  const reservations = await getReservation(params);
   const currentUser = await getCurrentUser();
 
   if (!listing) {
     <EmptyState showReset title="Invalid listing" subtitle="Try again later" />;
   }
 
-  return <ListingClient listing={listing} currentUser={currentUser} />;
+  return (
+    <ListingClient
+      listing={listing}
+      currentUser={currentUser}
+      reservations={reservations}
+    />
+  );
 }
