74677f7aabd08c7fa68fb1af27e1e916d4ddd0c1
RidloGhifary <ridloghfry@gmail.com>
2024-08-10 10:24:40 +0700

    feat: adding error handler page

---
+++ app/error.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import EmptyState from "@/components/EmptyState";
+import { useEffect } from "react";
+
+interface ErrorStateProps {
+  error: Error;
+}
+
+export default function ErrorState({ error }: ErrorStateProps) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  return <EmptyState title="Uh Oh" subtitle="Something went wrong" />;
+}
